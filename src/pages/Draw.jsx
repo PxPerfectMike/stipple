07c76cbb29a53310967474ccb405cca097ae8390
pxperfectmike <pxperfectmike@gmail.com>
2023-03-31 20:24:37 -0700

    add undo/redo/clear and save for refresh in localStorage

--- src/pages/Draw.jsx
+++ src/pages/Draw.jsx
@@ -1,39 +1,112 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { useState } from 'react';
-// import { Link } from 'react-router-dom';
+import React, { useState, useRef, useEffect } from 'react';
 
 function Draw() {
-	const [dotCount, setdotCount] = useState([]);
-	const [dotSize, setdotSize] = useState(1);
+	const [dotSize, setDotSize] = useState(1);
 	const sizes = [1, 4, 6, 8, 10, 12, 14];
+	const canvasRef = useRef(null);
+	const mouseIsDown = useRef(false);
+	const drawCtx = useRef(null);
+	const actions = useRef([]);
+	const [actionsChanged, setActionsChanged] = useState(false);
+
+	useEffect(() => {
+		drawCtx.current = canvasRef.current.getContext('2d');
+
+		// Load actions from local storage
+		const savedActions = localStorage.getItem('drawActions');
+		if (savedActions) {
+			actions.current = JSON.parse(savedActions);
+		}
+		redraw();
+	}, []);
+
+	useEffect(() => {
+		// Save actions to local storage when actionsChanged is true
+		if (actionsChanged) {
+			localStorage.setItem('drawActions', JSON.stringify(actions.current));
+			setActionsChanged(false);
+		}
+
+		// Save actions when the component is unmounted
+		return () => {
+			localStorage.setItem('drawActions', JSON.stringify(actions.current));
+		};
+	}, [actionsChanged]);
+
+	const redraw = () => {
+		drawCtx.current.clearRect(
+			0,
+			0,
+			canvasRef.current.width,
+			canvasRef.current.height
+		);
+		actions.current.forEach((action) => {
+			action.dots.forEach((dot) => {
+				drawCtx.current.beginPath();
+				drawCtx.current.arc(dot.x, dot.y, sizes[dot.size] / 2, 0, Math.PI * 2);
+				drawCtx.current.fillStyle = '#f5f5f5';
+				drawCtx.current.fill();
+			});
+		});
+	};
 
 	const handleAddDot = (event) => {
-		const rect = event.currentTarget.getBoundingClientRect();
+		const rect = canvasRef.current.getBoundingClientRect();
 		const x = event.clientX - rect.left;
 		const y = event.clientY - rect.top;
-		setdotCount((prevdotCount) => [
-			...prevdotCount,
-			{ x: x, y: y, size: dotSize },
-		]);
-		console.log(JSON.stringify(dotCount));
-		let interval;
-		const addDotAtInterval = () => {
-			interval = setInterval(() => {
-				setdotCount((prevdotCount) => [...prevdotCount, { x: x, y: y }]);
-			}, 1000); // <-- change this value to change the interval
-		};
+		const dot = { x, y, size: dotSize };
 
-		const stopAddingDots = () => {
-			clearInterval(interval);
-			document.removeEventListener('mouseup', stopAddingDots);
-		};
-		addDotAtInterval();
-		document.addEventListener('mouseup', stopAddingDots);
+		// Draw the dot immediately without redrawing the entire canvas
+		drawCtx.current.beginPath();
+		drawCtx.current.arc(x, y, sizes[dot.size] / 2, 0, Math.PI * 2);
+		drawCtx.current.fillStyle = '#f5f5f5';
+		drawCtx.current.fill();
+
+		actions.current[actions.current.length - 1].dots.push(dot);
+		setActionsChanged(true);
+	};
+
+	const maxUndoCapacity = 20; // <-- A greater maxUndoCapacity can me monetized
+
+	const handleMouseDown = (event) => {
+		mouseIsDown.current = true;
+		if (actions.current.length >= maxUndoCapacity) {
+			actions.current.shift();
+		}
+		actions.current.push({ dots: [] });
+		handleAddDot(event);
+	};
+
+	const handleMouseUp = () => {
+		mouseIsDown.current = false;
+	};
+
+	const handleMouseMove = (event) => {
+		if (mouseIsDown.current) {
+			handleAddDot(event);
+		}
+	};
+
+	const undoneActions = useRef([]);
+
+	const handleUndo = () => {
+		if (actions.current.length > 0) {
+			const lastAction = actions.current.pop();
+			undoneActions.current.push(lastAction);
+			redraw();
+		}
+	};
+
+	const handleRedo = () => {
+		if (undoneActions.current.length > 0) {
+			const lastUndoneAction = undoneActions.current.pop();
+			actions.current.push(lastUndoneAction);
+			redraw();
+		}
 	};
 
 	const handleIncrement = () => {
-		setdotSize((prevSize) => {
+		setDotSize((prevSize) => {
 			if (prevSize < sizes.length - 1) {
 				return prevSize + 1;
 			}
@@ -42,7 +115,7 @@ function Draw() {
 	};
 
 	const handleDecrement = () => {
-		setdotSize((prevSize) => {
+		setDotSize((prevSize) => {
 			if (prevSize > 0) {
 				return prevSize - 1;
 			}
@@ -50,9 +123,41 @@ function Draw() {
 		});
 	};
 
+	const handleClear = () => {
+		// Clear the canvas
+		drawCtx.current.clearRect(
+			0,
+			0,
+			canvasRef.current.width,
+			canvasRef.current.height
+		);
+
+		// Reset the actions and undoneActions
+		actions.current = [];
+		undoneActions.current = [];
+
+		// Set actionsChanged to true so the empty state is saved to local storage
+		setActionsChanged(true);
+	};
+
 	return (
-		<div className='App' onClick={handleAddDot}>
-			<p style={{ height: '80vh', userSelect: 'none' }}>Draw</p>
+		<div className='App'>
+			<p style={{ userSelect: 'none' }}>Draw</p>
+			<canvas
+				ref={canvasRef}
+				width={window.innerWidth / 1.2}
+				height={window.innerHeight / 1.2}
+				onMouseDown={handleMouseDown}
+				onMouseUp={handleMouseUp}
+				onMouseMove={handleMouseMove}
+				style={{
+					display: 'block',
+					float: 'right',
+					marginRight: '3%',
+					background: '#070606',
+					border: '3px solid #f5f5f5',
+				}}
+			></canvas>
 			<div className='button-group' style={{ zIndex: 2 }}>
 				<button style={{ userSelect: 'none' }} onClick={handleIncrement}>
 					+dotSize
@@ -62,24 +167,19 @@ function Draw() {
 					-dotSize
 				</button>
 			</div>
-			{dotCount.map((dot, index) => (
-				<motion.svg
-					key={index}
-					className='dot'
-					style={{
-						zIndex: 1,
-						left: dot.x,
-						top: dot.y,
-						width: `${sizes[dotSize]}px`,
-						height: `${sizes[dotSize]}px`,
-						backgroundColor: '#f5f5f5',
-					}}
-					initial={{ opacity: 0.7, scale: 0.9 }}
-					animate={{ opacity: 1, scale: 1 }}
-					exit={{ opacity: 0, scale: 0.5 }}
-				></motion.svg>
-			))}
-			<div className='draw-area'></div>
+			<div className='undo-redo-buttons'>
+				<button style={{ userSelect: 'none' }} onClick={handleUndo}>
+					Undo
+				</button>
+				<button style={{ userSelect: 'none' }} onClick={handleRedo}>
+					Redo
+				</button>
+			</div>
+			<div className='clear-button'>
+				<button style={{ userSelect: 'none' }} onClick={handleClear}>
+					Clear
+				</button>
+			</div>
 		</div>
 	);
 }
