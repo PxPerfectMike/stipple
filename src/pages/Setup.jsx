b2a9b4c59434ba5945784a0fce210b31b5ac6c31
pxperfectmike <pxperfectmike@gmail.com>
2023-04-02 15:06:53 -0700

    add color select for setup

--- src/pages/Setup.jsx
+++ src/pages/Setup.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { CirclePicker } from 'react-color';
+import { colorList } from '../assets/colors.js';
 
 function Setup() {
 	const [canvasDimensions, setCanvasDimensions] = useState({
 		width: 800,
 		height: 600,
 	});
-	const [backgroundColor, setBackgroundColor] = useState('#aaaaaa');
+	const [backgroundColor, setBackgroundColor] = useState('#90CAF9');
+	const [colorGroup, setColorGroup] = useState('Blue');
+
 	const navigate = useNavigate();
 
 	const handleSetCanvasSize = (width, height) => {
@@ -17,32 +21,6 @@ function Setup() {
 		setBackgroundColor(color);
 	};
 
-	const hexToRgb = (hex) => {
-		const bigint = parseInt(hex.slice(1), 16);
-		const r = (bigint >> 16) & 255;
-		const g = (bigint >> 8) & 255;
-		const b = bigint & 255;
-
-		return `rgb(${r}, ${g}, ${b})`;
-	};
-
-	const rgbToHex = (rgb) => {
-		const [r, g, b] = rgb
-			.substring(4, rgb.length - 1)
-			.replace(/ /g, '')
-			.split(',');
-
-		return (
-			'#' +
-			[r, g, b]
-				.map((x) => {
-					const hex = parseInt(x).toString(16);
-					return hex.length === 1 ? '0' + hex : hex;
-				})
-				.join('')
-		);
-	};
-
 	const handleStartDrawing = () => {
 		navigate('/draw', { state: { canvasDimensions, backgroundColor } });
 	};
@@ -51,49 +29,49 @@ function Setup() {
 		navigate(-1);
 	};
 
+	const colorNames = Object.keys(colorList);
+
 	return (
 		<div className='setup'>
 			<h2>Canvas Size</h2>
 			<div className='canvas-options'>
-				<button onClick={() => handleSetCanvasSize(800, 600)}>Landscape</button>
-				<button onClick={() => handleSetCanvasSize(600, 800)}>Portrait</button>
-				<button onClick={() => handleSetCanvasSize(600, 600)}>Square</button>
+				<button onClick={() => handleSetCanvasSize(900, 600)}>Landscape</button>
+				<button onClick={() => handleSetCanvasSize(600, 900)}>Portrait</button>
+				<button onClick={() => handleSetCanvasSize(1080, 1080)}>Square</button>
 			</div>
 			<h2>Background Color</h2>
+			<div className='color-buttons'>
+				{colorNames.map((colorName) => {
+					return (
+						<button
+							className='color-button'
+							key={colorName}
+							onClick={() => setColorGroup(colorName)}
+						>
+							{colorName}
+						</button>
+					);
+				})}
+			</div>
 			<div className='color-inputs'>
-				<input
-					type='color'
-					value={backgroundColor}
-					onChange={(e) => handleSetColor(e.target.value)}
-				/>
-				<input
-					type='text'
-					value={backgroundColor}
-					onChange={(e) => {
-						if (e.target.value.startsWith('#')) {
-							handleSetColor(e.target.value);
-						}
-					}}
-				/>
-				<input
-					type='text'
-					value={hexToRgb(backgroundColor)}
-					onChange={(e) => {
-						if (e.target.value.toLowerCase().startsWith('rgb')) {
-							handleSetColor(rgbToHex(e.target.value));
-						}
-					}}
+				<CirclePicker
+					color={backgroundColor}
+					colors={colorList[colorGroup]}
+					onChange={(color) => handleSetColor(color.hex)}
 				/>
 			</div>
-			<div className='canvas-preview'>
-				<div
-					className='preview'
-					style={{
-						width: `${canvasDimensions.width / 10}px`,
-						height: `${canvasDimensions.height / 10}px`,
-						backgroundColor: backgroundColor,
-					}}
-				></div>
+			<div className='canvas-preview-container'>
+				<div className='canvas-preview'>
+					<h2>Canvas Preview</h2>
+					<div
+						className='preview'
+						style={{
+							width: `${canvasDimensions.width / 8}px`,
+							height: `${canvasDimensions.height / 8}px`,
+							backgroundColor: backgroundColor,
+						}}
+					></div>
+				</div>
 			</div>
 			<div className='buttons'>
 				<button onClick={handleStartDrawing}>Start Drawing</button>
